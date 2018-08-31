ec2605de4db38c8950c25802c136f037bec3ebf7
Jihong Lee <jihonglee88@gmail.com>
2018-08-31 14:36:05 +0900

    'update'

---
+++ Chapter07/Recipe4/BackgroundJob/OrderProcessing.cs
@@ -0,0 +1,11 @@
+using System;
+namespace BackgroundJob
+    {
+    class OrderProcessing
+        {
+        public static void ProcessOrder()
+        {
+            Console.WriteLine("Process Order method of OrderProcessing class has been called");
+        }
+    }
+}
